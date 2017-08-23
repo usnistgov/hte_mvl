8ea31e54f12be051a2cb616e919607bfab476ae2
Xavier SCHMITT <xavier.schmitt@nist.gov>
2017-08-23 13:54:28 -0400

    add workspace url
    Change-Id: I8ad6b5cccd0fe7fec682264d01eccf5082243fa0

--- hte_mvl/urls.py
+++ hte_mvl/urls.py
@@ -38,4 +38,5 @@ urlpatterns = [
     url(r'^oai_pmh/server/', include("core_oaipmh_provider_app.urls")),
     url(r'^docs/api', schema_view),
     url(r'^tsne/', include("core_tsne_app.urls")),
+    url(r'^workspace/', include("core_workspace_app.urls")),
 ]
